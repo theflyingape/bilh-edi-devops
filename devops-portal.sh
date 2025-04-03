cbc26ee3748b4e58513772b886caa8270258384f
Robert Hurst <theflyingape@gmail.com>
2025-04-02 20:58:16 -0400

    redo devops portal service startup scripts

---
+++ devops-portal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /opt/devops
+
+NODE_VERSION=lts/iron NODE_ENV=production .nvm/nvm-exec npm run build
+
+export NITRO_SSL_CERT="`cat /etc/pki/tls/certs/hcie-san.crt /etc/pki/tls/certs/hcie-san.int`"
+export NITRO_SSL_KEY="`cat /etc/pki/tls/private/hcie-san.kry`"
+PORT=6500 .nvm/nvm-exec node .output/server/index.mjs
