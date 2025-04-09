4490bb5934ee8b5b0c0ae7cc0aaf69390354ac90
Robert Hurst <theflyingape@gmail.com>
2025-04-09 08:46:13 -0400

    delay execution

--- app/pages/code.vue
+++ app/pages/code.vue
@@ -3,15 +3,27 @@
     <UCard class="drop-shadow-2xl" variant="subtle">
       <template #header>
         <div class="flex justify-end text-xl">
-          <i>"Use this PIN when prompted for PASSWORD."</i>
-          <UIcon name="i-vscode-icons-file-type-robots" class="align-middle size-12" />
+          <div v-if="pin.length">
+            <i>"Use this PIN as your PASSWORD."</i>
+            <UIcon name="i-vscode-icons-file-type-robots" class="align-middle size-12" />
+          </div>
+          <div v-else>
+            <i>"Hello. May I prepare a working session for you?"</i>
+            <UIcon name="i-vscode-icons-file-type-robots" class="align-middle size-12" />
+          </div>
         </div>
       </template>
       <template #default>
         <div class="flex flex-col gap-4 items-center">
-          <UPinInput v-model="pin" type="number" length="3" disabled />
-          <UButton class="text-2xl" icon="i-vscode-icons-file-type-vscode" color="info" variant="soft" target="_blank" :to="url" :label="`Start hacking`" />
-        </div>
+          <div v-if="pin.length">
+            <UPinInput v-model="pin" type="number" length="3" disabled />
+            <UButton class="text-2xl" icon="i-vscode-icons-file-type-vscode" color="info" variant="soft" target="_blank" :to="url" :label="`Start hacking`" />
+          </div>
+          <div v-else class="space-y-8">
+            <div class=" font-semibold text-2xl  text-sky-600 text-center"><UIcon class="align-middle" name="i-vscode-icons-file-type-vscode" size="48" />&nbsp;Code Server</div>
+            <SubmitButton class="text-2xl" color="action" target="_blank" :to="url" :label="`Prepare my session`" @click="execute" />
+          </div>
+      </div>
       </template>
       <template #footer>
         <div class="text-start text-lg">{{ now }}</div>
@@ -27,19 +39,16 @@ const now = useNow()
 const pin = ref([])
 const url = ref('')
 const username = get(data)?.id
+const { execute, onFetchResponse } = useFetch(`/api/code-server?username=${username}`, { immediate: false} )
 
-onMounted(() => {
-  const { onFetchResponse } = useFetch(`/api/code-server?username=${username}`)
-
-  onFetchResponse((response) => {
-    response.json().then((value) => {
-      console.log('code-server response:', JSON.stringify(value))
-      if (value?.status == 'OK') {
-        set(pin, value?.pin)
-        set(url, value?.url)
-        //navigateTo(value?.url, { open: { target: '_blank' } })
-      }
-    })
+onFetchResponse((response) => {
+  response.json().then((value) => {
+    console.log('code-server response:', JSON.stringify(value))
+    if (value?.status == 'OK') {
+      set(pin, value?.pin)
+      set(url, value?.url)
+      //navigateTo(value?.url, { open: { target: '_blank' } })
+    }
   })
 })
 </script>
