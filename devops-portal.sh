7e3550d1f63667c5223f90212b88d31de6ac855f
Robert Hurst <theflyingape@gmail.com>
2025-04-18 13:25:22 -0400

    mishandled env

--- devops-portal.sh
+++ devops-portal.sh
@@ -6,5 +6,4 @@ NODE_VERSION=lts/iron NODE_ENV=production .nvm/nvm-exec npm run build
 
 export NITRO_SSL_CERT="`cat /etc/pki/tls/certs/hcie-san.crt /etc/pki/tls/certs/hcie-san.int`"
 export NITRO_SSL_KEY="`cat /etc/pki/tls/private/hcie-san.key`"
-[ -f .env ] && source .env
 PORT=6500 .nvm/nvm-exec node .output/server/index.mjs
