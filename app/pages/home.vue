ef0d02a391b5cb232e9dd9439e357ad2117fe13d
Robert Hurst <theflyingape@gmail.com>
2025-05-03 11:05:01 -0400

    home page future card layout

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,12 +1,54 @@
 <template>
-  <div v-if="online && isAdmin" class="flex justify-center">
-    <div class="grid grid-flow-col grid-rows-2">
-      <UPlaceholder class="row-span-2 shrink">
-        <UCard><em>pending 2024 upgrade</em></UCard>
-        <!--IrisMirrorStatus hcie="Live" /-->
-      </UPlaceholder>
-      <IrisMirrorStatus hcie="Test" />
-      <IrisMirrorStatus hcie="Dev" />
+  <div v-if="online" class="m-1">
+    <div class="grid grid-cols-2 gap-8">
+      <div class="grid auto-rows-auto gap-4">
+        <div class="text-center underline">LIVE</div>
+        <div class="flex justify-center gap-2">
+          <UCard variant="outline"><em>future card</em></UCard>
+          <UCard variant="outline"><em>future card</em></UCard>
+          <UCard variant="outline"><em>future card</em></UCard>
+        </div>
+        <div class="flex justify-center gap-2">
+          <UCard variant="soft"><em>future card</em></UCard>
+          <UCard variant="solid"><em>future card</em></UCard>
+        </div>
+        <div class="flex justify-center gap-2">
+          <UCard variant="subtle"><em>future card</em></UCard>
+        </div>
+      </div>
+      <div class="grid auto-rows-auto gap-4">
+        <div class="text-center underline">TEST</div>
+        <div class="flex justify-center gap-2">
+          <UCard variant="soft"><em>future card</em></UCard>
+          <UCard variant="solid"><em>future card</em></UCard>
+        </div>
+        <div class="text-center underline">DEVELOPMENT</div>
+        <div class="flex justify-center gap-2">
+          <UCard variant="subtle"><em>future card</em></UCard>
+        </div>
+      </div>
+    </div>
+    <div v-if="isAdmin" class="m-1">
+      <USeparator class="h-4" color="action" orientation="horizontal" type="dotted" />
+      <div class="grid grid-cols-2">
+        <div class="space-y-4">
+          <div class="flex justify-center">
+            <UCard>
+              <div class="font-bold font-sans underline">LIVE</div>
+              <em>pending 2024 upgrade</em>
+            </UCard>
+            <!--IrisMirrorStatus hcie="Live" /-->
+          </div>
+        </div>
+        <div class="space-y-4">
+          <div class="flex justify-center">
+            <IrisMirrorStatus hcie="Test" />
+          </div>
+          <div class="flex justify-center">
+            <IrisMirrorStatus hcie="Dev" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div v-else class="flex flex-col items-center justify-center h-lvh">
