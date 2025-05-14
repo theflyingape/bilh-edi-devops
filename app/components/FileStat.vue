b2f588c5527ed034fb382320dc6fea3047043143
Robert Hurst <theflyingape@gmail.com>
2025-05-14 16:52:07 -0400

    reorg FileStat card

--- app/components/FileStat.vue
+++ app/components/FileStat.vue
@@ -4,13 +4,11 @@
       <div class="text-sm font-mono">
         <div>{{ fileStat[props.hcie]?.owner ?? 'owner' }}:{{ fileStat[props.hcie]?.group ?? 'group' }}</div>
         <div>{{ fileStat[props.hcie]?.size }} bytes</div>
-        <div>{{ fileStat[props.hcie]?.type }} type</div>
         <div>{{ new Date(fileStat[props.hcie]!.modified).toLocaleString('en-US', { dateStyle:'medium', timeStyle:'short' }) }}</div>
       </div>
-    </template>
-    <template #footer>
       <div class="flex font-mono justify-start italic text-sm">
-        <UTextarea v-model="fileStat[props.hcie]!.fileName" disabled autoresize color="neutral" icon="i-lucide-file-text" placeholder="highlight file..." :rows="1" size="sm" variant="subtle" />
+        <UTextarea v-if="fileStat[props.hcie]?.type == 'regular file'" v-model="fileStat[props.hcie]!.fileName" disabled autoresize color="neutral" icon="i-lucide-file-text" placeholder="highlight file..." :rows="1" size="sm" variant="subtle" />
+        <div v-else>{{ fileStat[props.hcie]?.type }}</div>
       </div>
     </template>
   </UCard>
