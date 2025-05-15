717132efd5bdceb8c0765f2386f73ab358db3687
Robert Hurst <theflyingape@gmail.com>
2025-05-15 08:51:59 -0400

    replace dynamic object with Map

--- app/composables/useIrisSessions.ts
+++ app/composables/useIrisSessions.ts
@@ -10,22 +10,18 @@ export interface IRIStoken {
 }
 
 export interface User {
-  id: string,
-  enabled: boolean,
-  groups?: string[],
-  roles?: string[],
-  name?: string,
-  comment?: string,
-  loggedInAt?: number,
+  id: string
+  enabled: boolean
+  groups?: string[]
+  roles?: string[]
+  name?: string
+  comment?: string
+  loggedInAt?: number
   scope: Array<'guest' | 'user' | 'analyst' | 'developer' | 'admin' | 'systems'>
 }
 
 //  keep tokens by HCIE endpoint
-interface tokens {
-  [key: string]: IRIStoken
-}
-
-let tokens: tokens = {}
+const tokens = new Map<string, IRIStoken>()
 
 export interface mirrorstatus {
   memberName: string
@@ -50,17 +46,19 @@ export interface mirrorset {
   lastBackup: string
 }
 
-const mirrorSet:Ref<{[key: string]: mirrorset}> = ref({ Dev:<mirrorset>{}, Test:<mirrorset>{}, Live:<mirrorset>{} })
+const mirrorSet: Ref<{ [key: string]: mirrorset }>
+  = ref({ Dev: <mirrorset>{}, Test: <mirrorset>{}, Live: <mirrorset>{} })
 
 export interface processes {
-  Production:string
-  File:number
-  Net:number
-  Queue:number
-  Misc?:number
+  Production: string
+  File: number
+  Net: number
+  Queue: number
+  Misc?: number
 }
 
-const processList:Ref<{[key: string]: processes}> = ref({ Dev:<processes>{}, Test:<processes>{}, Live:<processes>{} })
+const processList: Ref<{ [key: string]: processes }>
+  = ref({ Dev: <processes>{}, Test: <processes>{}, Live: <processes>{} })
 
 export interface filestat {
   size: number
@@ -72,25 +70,17 @@ export interface filestat {
   type: string
 }
 
-const fileStat:Ref<{[key: string]: filestat}> = ref({ Dev:<filestat>{}, Test:<filestat>{}, Live:<filestat>{} })
+const fileStat: Ref<{ [key: string]: filestat }>
+  = ref({ Dev: <filestat>{}, Test: <filestat>{}, Live: <filestat>{} })
 
-const HCIE: { [key:string]: string } = {
-  Dev: "hciedev.laheyhealth.org",
-  Test: "hcietst.laheyhealth.org",
-  Live: "hcieprd.laheyhealth.org"
-}
+const API: { [key: string]: string }
+  = { Dev: '/api/hcie', Test: '/api/hcie', Live: '/api/hcie' }
 
-const ICON: {[key: string]: string} = {
-  Dev: "i-vscode-icons-file-type-apib",
-  Test: "i-vscode-icons-file-type-light-todo",
-  Live: "i-vscode-icons-file-type-plsql-package"
-}
+const HCIE: { [key: string]: string }
+  = { Dev: 'hciedev.laheyhealth.org', Test: 'hcietst.laheyhealth.org', Live: 'hcieprd.laheyhealth.org' }
 
-const API: {[key: string]: string} = {
-  Dev: "/api/hcie",
-  Test: "/api/hcie",
-  Live: "/api/hcie"
-}
+const ICON: { [key: string]: string }
+  = { Dev: 'i-vscode-icons-file-type-apib', Test: 'i-vscode-icons-file-type-light-todo', Live: 'i-vscode-icons-file-type-plsql-package' }
 
 const credentials = ref({
   username: '',
@@ -102,22 +92,29 @@ const user = ref(<User>{})
 const dev = import.meta.dev || false
 
 export default function useIrisTokens() {
-  //  provision an IRIS REST JWT session off user authentication
-  async function getSession(hcie: string, username: string, password: string): Promise<IRIStoken | null> {
-    let IRIStoken = dev ? { access_token: 'access', refresh_token: 'refresh', sub: username, iat: 0, exp: -1 } : null
-    if (IRIStoken) {
+  //  provision a new IRIS REST JWT session off user authentication
+  async function getSession(hcie: string, username: string, password: string): Promise<IRIStoken | undefined> {
+    let iToken: IRIStoken | undefined = dev
+      ? {
+          access_token: 'access',
+          refresh_token: 'refresh',
+          sub: username,
+          iat: 0,
+          exp: -1
+        }
+      : undefined
+    if (iToken) {
       set(user, {
         id: get(credentials).username,
         enabled: true,
         groups: ['sysadm', 'irisadm', 'irisdev'],
         roles: ['%Manager', '%Developer', '%Operator'],
-        name: 'Devlin Tester',
-        comment: 'TPM',
+        name: 'Devlin Opster',
+        comment: 'Master Inventor',
         loggedInAt: Date.now(),
         scope: []
       })
-    }
-    else {
+    } else {
       const auth = btoa(`${username}:${password}`)
       await fetch(`https://${HCIE[hcie]}${API[hcie]}/login`, {
         method: 'POST',
@@ -125,34 +122,34 @@ export default function useIrisTokens() {
       }).then(async (res) => {
         try {
           await res.json().then(async (t) => {
-            tokens[hcie] = t
-            IRIStoken = t
+            tokens.set(hcie, t)
+            iToken = tokens.get(hcie)
           })
-        }
-        catch (err) {
+        } catch (err) {
           console.error(err)
         }
       })
     }
-    return IRIStoken
+    return iToken
   }
 
   //  bye-bye
   async function endSession(hcie: string) {
     if (dev) return
-    const jwt = tokens[hcie]
+    const jwt = tokens.get(hcie)
     if (jwt) {
       await fetch(`https://${HCIE[hcie]}${API[hcie]}/logout`, {
         method: 'POST',
         headers: { Authorization: `Bearer ${jwt.access_token}` }
       }).then(async (res) => {
-        delete tokens[hcie]
+        tokens.delete(hcie)
       })
     }
   }
 
-  async function refresh(hcie: string, jwt: IRIStoken) {
+  async function refresh(hcie: string) {
     if (dev) return
+    const jwt = tokens.get(hcie)
     if (jwt && jwt.exp) {
       const secs = jwt.exp - Date.now() / 1000
       if (secs > 2) return
@@ -161,44 +158,42 @@ export default function useIrisTokens() {
     await fetch(`https://${HCIE[hcie]}${API[hcie]}/refresh`, {
       method: 'POST',
       headers: {
-        Authorization: `Bearer ${jwt.access_token}`,
+        'Authorization': `Bearer ${jwt?.access_token}`,
         'Content-Type': 'application/json'
       }, body: JSON.stringify(jwt)
     }).then(async (res) => {
       try {
         await res.json().then(async (jwt) => {
-          tokens[hcie] = jwt
+          tokens.set(hcie, jwt)
         })
-      }
-      catch (err) {
+      } catch (err) {
         console.error(err)
       }
     })
   }
 
-  async function endpoint(hcie: string, route: string, method:'GET'|'POST' = 'GET', send:object|undefined): Promise<any|null> {
+  async function endpoint(hcie: string, route: string, method: 'GET' | 'POST' = 'GET', send: object | undefined): Promise<any | null> {
     let payload = null
-    let jwt = tokens[hcie]
+    let jwt = tokens.get(hcie)
     if (dev) return payload
 
     //  upon first invocation to another instance ...
     if (!jwt) {
       await getSession(hcie, get(credentials).username, get(credentials).password).then((login) => {
-        if(login) {
-          jwt = login
-          tokens[hcie] = jwt
+        if (login) {
+          tokens.set(hcie, login)
         }
       })
     }
 
     //  let's refresh access prior to invocation
     if (jwt) {
-      await refresh(hcie, jwt).then(async () => {
-        jwt = tokens[hcie]!
+      await refresh(hcie).then(async () => {
+        jwt = tokens.get(hcie)
         await fetch(`https://${HCIE[hcie]}${API[hcie]}/${route}`, {
           method: method,
           headers: {
-            Authorization: `Bearer ${jwt.access_token}`,
+            'Authorization': `Bearer ${jwt?.access_token}`,
             'Content-Type': 'application/json'
           },
           body: send ? JSON.stringify(send) : undefined
@@ -207,8 +202,7 @@ export default function useIrisTokens() {
             await res.json().then(async (js) => {
               payload = js
             })
-          }
-          catch (err) {
+          } catch (err) {
             console.error(err)
           }
         })
@@ -217,9 +211,8 @@ export default function useIrisTokens() {
     return payload
   }
 
-
-  async function stat(hcie:string, fileName:string) {
-    await endpoint(hcie, 'filestat', 'POST', { fileName: fileName }).then((result:filestat) => {
+  async function stat(hcie: string, fileName: string) {
+    await endpoint(hcie, 'filestat', 'POST', { fileName: fileName }).then((result: filestat) => {
       fileStat.value[hcie] = result
     })
   }
