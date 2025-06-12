b2264c156ee208a094b43704f6f72e9dccb09efd
Robert Hurst <theflyingape@gmail.com>
2025-06-12 08:45:41 -0400

    allow systems too

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,15 +2,15 @@ import { get } from '@vueuse/core'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   try {
+    const { isAdmin } = useDevOps()
     const { user } = useIrisSessions()
     const notAdmin = ['admin']
     const notAllowed = ['code', 'terminal']
 
     //  a little negative, but steer any visitors away ...
-    if (notAdmin.includes(to.name!.toString())) {
-      if (!get(user)?.enabled || !get(user)?.scope?.length || get(user).scope[0] !== 'admin')
-        return navigateTo('/sorry')
-    }
+    if (notAdmin.includes(to.name!.toString()) && !isAdmin)
+      return navigateTo('/sorry')
+
     if (notAllowed.includes(to.name!.toString())) {
       if (!get(user)?.enabled || !get(user)?.scope?.length || get(user).scope[0] == 'guest' || get(user).scope[0] == 'user')
         return navigateTo('/sorry')
