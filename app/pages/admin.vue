99c0a3ea462aa54425d620176482f2d331a94cc1
Robert Hurst <theflyingape@gmail.com>
2025-06-12 07:44:00 -0400

    admin page

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -1,10 +1,21 @@
-<!-- eslint-disable vue/valid-template-root -->
+<!-- eslint-disable vue/max-attributes-per-line -->
+<!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
+  <div v-if="online" class="m-1">
+    <div class="grid grid-cols-2 gap-8">
+      <div class="grid auto-rows-auto gap-4">
+        <div class="font-bold text-center">USERS</div>
+        <div class="flex justify-center gap-2">
+          insert table here
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-  auth: false,
   pageTransition: { name: 'page', mode: 'out-in' }
 })
+const { online } = useDevOps()
 </script>
