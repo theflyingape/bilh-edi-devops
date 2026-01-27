7c6ba5da5ac0adfa6586f9595cf822e84258f1c9
Robert Hurst <theflyingape@gmail.com>
2026-01-27 11:26:56 -0500

    upgraded to Nuxt v4.3.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
   },
   appConfig: {
     buildDate: new Date().toLocaleString('en-us', { dateStyle: 'full', timeStyle: 'short' }),
-    version: '0.9.9'
+    version: '0.9.10'
   },
   runtimeConfig: {
     jwtSecret: process.env.NUXT_JWT_PASSWORD || '!$ecure!',
