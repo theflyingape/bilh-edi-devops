92fe474f80f2ed72b3a35d7a3b3cbc51fd078f73
Robert Hurst <theflyingape@gmail.com>
2026-05-15 12:59:57 -0400

    colorize

--- app/components/TokensByUser.vue
+++ app/components/TokensByUser.vue
@@ -5,8 +5,8 @@
       <div class="grid grid-cols-6 gap-3">
         <div v-for="(ts, index) in portal.logins" :key="index" class="col-span-2">
           <UTooltip :text="ago(new Date(ts).toLocaleString()) || 'not known'" arrow :content="{ side: 'right', sideOffset: 2 }">
-            <UChip class="w-full" :color="aging[index]" inset size="xs">
-              <UBadge class="p-2 rounded-full justify-center text-neutral min-w-[120px] w-full" :color="aging[index]" variant="soft">
+            <UChip class="w-full" :color="aging(ts)" inset size="xs">
+              <UBadge class="p-2 rounded-full justify-center text-neutral min-w-[120px] w-full" :color="aging(ts)" variant="soft">
                 {{ portal.online[index] }}
               </UBadge>
             </UChip>
@@ -15,19 +15,22 @@
       </div>
     </template>
     <template #footer>
-      <div class="justify-self-end text-sm"><i>Visitors over the past 3 months</i></div>
+      <div class="justify-self-end text-sm">
+        <i>Visitors over the past 3 months</i>
+      </div>
     </template>
   </UCard>
 </template>
 
 <script setup lang="ts">
-import { get, set } from '@vueuse/core'
-
 const { ago, portal, who } = useDevOps()
-const aging = shallowRef(<Array<'primary' | 'secondary' | 'neutral'>>[])
+
+function aging(ts: number): 'primary' | 'secondary' | 'neutral' {
+  console.log(ts)
+  return (Date.now() - ts) < (6 * 60 * 60 * 1000) ? 'primary' : (Date.now() - ts) < (12 * 60 * 60 * 1000) ? 'secondary' : 'neutral'
+}
 
 onMounted(() => {
   who()
-  set(aging, get(portal).logins.map(val => (Date.now() - val) < (6 * 60 * 60 * 1000) ? 'primary' : (Date.now() - val) < (12 * 60 * 60 * 1000) ? 'secondary' : 'neutral'))
 })
 </script>
